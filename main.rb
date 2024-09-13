4cfc36f55c7eb01a2cd00307e2ff6e1772bc6233
Je Rome <jeromeabelida19@gmail.com>
2024-09-13 14:25:38 +0800

    Done Repeat Actions using a Loop

--- main.rb
+++ main.rb
@@ -52,50 +52,71 @@ persons = [
   }
 ]
 
-p "Type Delete or Add"
-chosen_word = gets.chomp.upcase
+p "Want to add or delete> please type Y/N"
+input = gets.chomp.upcase
+start = false
+
+if input == "Y"
+  start = true
+end
+
+while start
+  p "Type Delete or Add"
+  chosen_word = gets.chomp.upcase
 
 
-if chosen_word == "DELETE"
-  p "Input the id you want to delete"
-  deleting_id = gets.chomp.to_i
-  id_exist = persons.any?{|element|element[:national_id] == deleting_id}
-  persons.each_with_index do |element,index|
-    if id_exist
-      p "successfully deleted"
-      persons.delete_at(index)
-      puts persons
-      break
-    else
-      p "user not found"
-      break
+  if chosen_word == "DELETE"
+    p "Input the id you want to delete"
+    deleting_id = gets.chomp.to_i
+    id_exist = persons.any?{|element|element[:national_id] == deleting_id}
+
+    persons.each_with_index do |element,index|
+      if persons[index][:national_id] == deleting_id
+        persons.delete_at(index)
+        p "successfully deleted"
+        puts persons
+        break
+      else
+        p "user not found"
+      end
+    end
+  elsif chosen_word == "ADD"
+
+    p "Please input a new national id: "
+    id = gets.chomp.to_i
+
+    p "Please input your name: "
+    name = gets.chomp
+
+    p "Please input your age: "
+    age = gets.chomp.to_i
+
+    id_exist = persons.any?{|element|element[:national_id] == id}
+
+    persons.each do |element|
+      if  id_exist
+        p "Failed To add: National ID already exist"
+        break
+      else
+        persons.push({
+                       national_id: id,
+                       name: name,
+                       age: age
+                     })
+        puts persons
+        break
+      end
     end
   end
-elsif chosen_word == "ADD"
-  p "Please input a new national id: "
-  id = gets.chomp.to_i
 
-  p "Please input your name: "
-  name = gets.chomp
+  p "Do you want to continue? Please type Y/N"
+  want_to_continue = gets.chomp.upcase
 
-  p "Please input your age: "
-  age = gets.chomp.to_i
-
-  id_exist = persons.any?{|element|element[:national_id] == id}
-
-  persons.each do |element|
-    if  id_exist
-      p "Failed To add: National ID already exist"
-      break
-    else
-      persons.push({
-                     national_id: id,
-                     name: name,
-                     age: age
-                   })
-      puts persons
-      break
-    end
+  if want_to_continue == "Y"
+    start = true
+  else
+    start = false
   end
 end
+
 
