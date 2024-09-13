9f58cc59d806c2c7aafd57d794a44573cff4a1ac
Je Rome <jeromeabelida19@gmail.com>
2024-09-13 13:48:58 +0800

    Done Create array of People

--- main.rb
+++ main.rb
@@ -1,1 +1,61 @@
-puts "Hello World"
+# Creating an array of people
+persons = [
+  {
+    national_id: 1,
+    name: "Jerome",
+    age: "24"
+  },
+  {
+    national_id: 2,
+    name: "Rosaline",
+    age: "40"
+  },
+  {
+    national_id: 3,
+    name: "Arcadio",
+    age: "50"
+  },
+  {
+    national_id: 4,
+    name: "Armea",
+    age: "17"
+  },
+  {
+    national_id: 5,
+    name: "Mark",
+    age: "22"
+  },
+  {
+    national_id: 6,
+    name: "Jericho",
+    age: "21"
+  },
+  {
+    national_id: 7,
+    name: "Riza",
+    age: "24"
+  },
+  {
+    national_id: 8,
+    name: "Rona",
+    age: "24"
+  },
+  {
+    national_id: 9,
+    name: "Bryan",
+    age: "24"
+  },
+  {
+    national_id: 10,
+    name: "Ruben",
+    age: "54"
+  }
+]
+
+persons.each.with_index do |element, index|
+
+  element.each_with_index do |k, v|
+    p element[:name]
+    break
+  end
+end
